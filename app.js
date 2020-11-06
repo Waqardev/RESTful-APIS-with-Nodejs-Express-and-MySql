f27dc1f1384a1bf4781ea7207ea042c48988e837
Waqar Ahmad <w.waqarahmad786@gmail.com>
2020-11-06 23:32:43 +0500

    API Test

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.get("/api", (req, res) => {
+  res.json({
+    status: 1,
+    message: "The REST API is Working",
+  });
+});
+
 const port = process.env.port || 5000;
 app.listen(port, () => {
   console.log(`The server is running on ${port}`);
