1420f55f463d6f61f08ee67bcff1f316ded13519
Waqar Ahmad <w.waqarahmad786@gmail.com>
2020-11-11 22:23:47 +0500

    User Post API

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const userRouter = require("./api/users/user.router");
 const app = express();
 
+// Test Api
 app.get("/api", (req, res) => {
   res.json({
     status: 1,
     message: "The REST API is Working",
   });
 });
+
+//To Convert json to javascript object
+app.use(express.json());
+
+app.use("/api/users", userRouter);
 
 const port = process.env.App_Port || 3000;
 app.listen(port, () => {
